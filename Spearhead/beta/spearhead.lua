ed0ceb3516c639f2be80cf08c1649e1dab708cbf
automated action <automated@action.com>
2024-12-01 20:03:05 +0000

    Automatic Update of Spearhead README

--- Spearhead/beta/spearhead.lua
+++ Spearhead/beta/spearhead.lua
@@ -1,5 +1,5 @@
 --[[
-        Spearhead Compile Time: 2024-11-13T12:17:14.109009
+        Spearhead Compile Time: 2024-12-01T20:03:04.911782
     ]]
 do --spearhead_events.lua
 
@@ -2153,7 +2153,7 @@ do -- DB
                                     functionString = functionString .. " { x=" .. vecpoint.x .. ", y=0,z=" .. vecpoint.z ..
                                     "},"
                                 end
-                                functionString = functionString .. "{0,1,0,1}, {0,1,0,1}, 1)"
+                                functionString = functionString .. "{0,1,0,1}, {0,0,0,1}, 1)"
 
                                 env.info(functionString)
 ---@diagnostic disable-next-line: deprecated
@@ -2165,7 +2165,7 @@ do -- DB
                                 end
                             else
                                 trigger.action.circleToAll(-1, baseId, { x = point.x, y = 0, z = point.z }, 2048,
-                                    { 1, 0, 0, 1 }, { 1, 0, 0, 0 }, 1, true)
+                                    { 1, 0, 0, 1 }, { 0, 0, 0, 0 }, 1, true)
                             end
                         end
 
